fa70cf6fe2d54e31ac86008f09d8d87f82f8c63c
Awasthii <108925941+Awasthii@users.noreply.github.com>
2022-08-22 13:13:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['919955607892'] //ur owner number
+global.ownername = "🦄awasthii" //ur owner name
+global.ytname = "instgram: " feature.glows //ur yt chanel name
+global.socialm = "GitHub: Awasthi - feature.glows " //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['919955607892']
+global.ownertag = '919955607892' //ur tag number
+global.botname = 'Awasthi ' //ur bot name
+global.linkz =  https://instagram.com/feature.glows?igshid=YmMyMTA2M2Y=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/feature.glows?igshid=YmMyMTA2M2Y=" //ur website to be displayed
+global.botscript = 'https://instagram.com/feature.glows?igshid=YmMyMTA2M2Y=' //instgram link
+global.reactmoji = "🙂" //ur menu react emoji
+global.themeemoji = "🙂" //ur theme emoji
+global.packname = "INSTA - FEATURE.GLOWS  " //ur sticker watermark packname
+global.author = "INSTA - FEATURE.GLOWS" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -128,7 +128,7 @@ global.mess = {
    global.rpg = {
    darahawal: 100,
    besiawal: 15,
-   goldawal: 10,
+   goldawal: 10, 
    emeraldawal: 5,
    umpanawal: 5,
    potionawal: 1
